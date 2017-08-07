d4909800e8799fe9df6c4142b666d0156570cb15
Erivan Rodrigues <erivanrgj@gmail.com>
2017-08-06 21:18:29 -0300

    Remove console.logs from reducer upvotes
    Signed-off-by: Erivan Rodrigues <erivanrgj@gmail.com>

--- frontend/src/reducers/reducer_upvotes.js
+++ frontend/src/reducers/reducer_upvotes.js
@@ -3,8 +3,6 @@ import { FETCH_UPVOTES } from '../actions/index';
 export default function(state = [], action) {
   switch(action.type) {
     case FETCH_UPVOTES:
-      console.log('up_votes', action.pauload)
-      console.log('upvotes', action.payload.data.map((up_votes) => { return up_votes.comic_id }));
       return action.payload.data.map((up_votes) => { return up_votes.comic_id });
     default: return state;
   }
